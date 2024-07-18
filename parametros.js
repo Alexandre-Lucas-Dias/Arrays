53c2b10b4078095e6e8aa2cff9249ee986ec7689
Alexandre-Lucas-Dias <alexandrelucasdiascosta@yahoo.com.br>
2024-07-18 13:25:05 -0300

    atualizando

--- parametros.js
+++ parametros.js
@@ -23,3 +23,31 @@ function multiplica(numero1 = 1, numero2 = 1){
 }
 //............................9...........6
 console.log(multiplica(soma(4, 5), soma(3, 3)))
+
+function cumprimentarSimples(){
+    console.log('oi gente!')
+}
+   
+cumprimentarSimples()
+
+function cumprimentaPessoaSimples(pessoa){
+    console.log(`oi, ${pessoa}!`)
+}
+   
+cumprimentaPessoaSimples('Helena')
+
+function cumprimentar(){
+    return 'Oi gente!'
+}
+   
+function cumprimentaPessoa(nomePessoa) {
+    console.log(`${cumprimentar()} Meu nome é ${nomePessoa}`)
+}
+   
+cumprimentaPessoa('Paula') // “Oi gente! Meu nome é Paula”
+
+function operacaoMatematica(numero1, numero2, numero3) {
+    return numero1 + numero2 + numero3
+}
+   
+console.log(operacaoMatematica(15, 30, 45)) // 90
